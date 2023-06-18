eb0978ff47b76dcbea37d428d71cb2e6bb427dfa
Raphael Pacheco <raphaelrogen@gmail.com>
2023-06-18 18:06:06 -0300

    requisito 03

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,35 @@
-# Req 3
+import csv
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        self.load_dishes(source_path)
+
+    def load_dishes(self, source_path: str) -> None:
+        with open(source_path, "r") as file:
+            reader = csv.reader(file)
+            next(reader)
+
+            for x in reader:
+                dish_name, dish_price, ingredient_name, ingredient_quantity = x
+                dish = self.get_or_create_dish(
+                    dish_name,
+                    float(dish_price),
+                )
+                ingredient = Ingredient(ingredient_name)
+                dish.add_ingredient_dependency(
+                    ingredient,
+                    int(ingredient_quantity),
+                )
+
+    def get_or_create_dish(self, name: str, price: float) -> Dish:
+        for existing_dish in self.dishes:
+            if existing_dish.name == name:
+                return existing_dish
+
+        new_dish = Dish(name, price)
+        self.dishes.add(new_dish)
+        return new_dish
